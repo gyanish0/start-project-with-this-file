d436036435fabb3cfee7a81448ba05bf2edbcd94
gyanish77 <gyanish@luminoguru.com>
2022-06-09 16:23:19 +0530

    .push

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PageLoading from "./components/PageLoading";
-import AllRoutes from "./AllRoutes";
+import AllRoutes from "./routes";
+import { Toaster } from "react-hot-toast";
 // import AboutLayout from "./layouts/AboutLayout";
 function App() {
   // const name = window.$name;
@@ -13,6 +14,7 @@ function App() {
       <Suspense fallback={<PageLoading />}>
         <AllRoutes />
       </Suspense>
+      <Toaster position="top-right" reverseOrder={true} />
       <ToastContainer />
     </>
   );
--- src/views/pages/news/News.js
+++ src/views/pages/news/News.js
@@ -1,15 +1,106 @@
+import { Button } from "@material-ui/core";
 import React from "react";
-import Pagination from "./Pagination";
-import Search from "./Search";
-import Stories from "./Stories";
+import toast, { Toaster } from "react-hot-toast";
+// import Pagination from "./Pagination";
+// import Search from "./Search";
+// import Stories from "./Stories";
+const clickGandler = () => {
+  toast.success("Successfully toasted!");
+};
+const currentDate = new Date();
+const monthNumber = currentDate.getMonth();
+console.log(monthNumber);
+const m = [
+  {
+    id: 1,
+    name: "January",
+  },
+  {
+    id: 2,
+    name: "February",
+  },
+  {
+    id: 3,
+    name: "March",
+  },
+  {
+    id: 4,
+    name: "April",
+  },
+  {
+    id: 5,
+    name: "May",
+  },
+  {
+    id: 6,
+    name: "June",
+  },
+  {
+    id: 7,
+    name: "July",
+  },
+  {
+    id: 8,
+    name: "August",
+  },
+  {
+    id: 9,
+    name: "September",
+  },
+  {
+    id: 10,
+    name: "October",
+  },
+  {
+    id: 11,
+    name: "November",
+  },
+  {
+    id: 12,
+    name: "December",
+  },
+];
+var moo = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
+var cur = new Date().getMonth();
+
+var sorted = [...m.slice(cur), ...m.slice(0, cur)];
+
+console.log(sorted);
 const News = () => {
   return (
     <div>
-      <h1>News</h1>
-      <Search />
-      <Pagination />
-      <Stories />
+      <div></div>
+      <h1>Form</h1>
+      <Button onClick={clickGandler} variant="outlined">
+        Click
+      </Button>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        {/* {months.map((data) => (
+          <div
+            key={data.id}
+            style={{
+              color: cur === data.name ? "red" : "#000",
+              display: "flex",
+            }}
+          >
+            <h4>{data.name}</h4>
+          </div>
+        ))} */}
+      </div>
     </div>
   );
 };
